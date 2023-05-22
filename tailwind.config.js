c121e43aeebbcf2017cec8f54e59b7804777b0d5
Musubi42 <raphael.dev@protonmail.com>
2023-05-22 13:53:05 +0200

    Add images

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        header: "url('/assets/images/BG_header.png')",
+        content: "url('/assets/images/BG_content.png')",
+        "logo-oms": "url('/assets/images/Logo_OMS.png')",
+        hr: "url('/assets/svg/HR.svg')",
+      },
+    },
   },
   plugins: [],
 };
