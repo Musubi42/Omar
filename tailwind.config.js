3aad19f777dc8364f1fa0efc607a52b2d0d88cf8
Musubi42 <raphael.dev@protonmail.com>
2023-05-22 15:59:43 +0200

    Add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./src/*.html", "./public/**/*.html"],
   theme: {
     extend: {
       backgroundImage: {
-        header: "url('/assets/images/BG_header.png')",
+        header: "url('../assets/images/BG_header.png')",
         content: "url('/assets/images/BG_content.png')",
         "logo-oms": "url('/assets/images/Logo_OMS.png')",
         hr: "url('/assets/svg/HR.svg')",
+        "button-border": "url('/assets/svg/button_border.svg')",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        "lovelo-black": ["Lovelo_Black", "sans-serif"],
+        "lovelo-bold": ["Lovelo_Bold", "arial"],
+      },
+      colors: {
+        "active-button": "#00BF63",
       },
     },
   },
